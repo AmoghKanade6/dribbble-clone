8530dd0fc743d19699482679b6c8b9e8147ff966
Pooja S Kanade <amoghkanade4@gmail.com>
2023-12-06 19:32:57 +0530

    latsdfes

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,25 +1,20 @@
 import { g, auth, config } from "@grafbase/sdk";
 // @ts-ignore
-const User = g
-  .model("User", {
+const User = g.model("User", {
     name: g.string().length({ min: 2, max: 20 }),
     email: g.string().unique(),
     avatarUrl: g.url(),
     description: g.string().optional(),
     githubUrl: g.url().optional(),
     linkedinUrl: g.url().optional(),
-    projects: g
-      .relation(() => Project)
-      .list()
-      .optional(),
+    projects: g.relation(() => Project).list().optional(),
   })
   .auth((rules) => {
     rules.public().read();
   });
 
 // @ts-ignore
-const Project = g
-  .model("Project", {
+const Project = g.model("Project", {
     title: g.string().length({ min: 3 }),
     description: g.string(),
     image: g.url(),
